19de849a1905ef59964c4d9737d139e8236fa993
Charles Julian Knight <charles@rabidaudio.com>
2013-11-02 23:52:32 -0400

    cleaned up notes in MARKDOWN format, added sample app

---
+++ tutorial4/TestApp/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TestApp::Application.config.secret_key_base = '7a34b07bf4d1e93a66257ad2aa8f7fc26cce79f3f14aea0f9d8660b8f3820fa16ec909ddbfcde19997a2a46b608a409df5a6b3356059108c009a94350be8454b'
